50d69771fff02e7b086a62e14d08e377f861152f
Shlee <github@shl.ee>
2026-08-31 18:36:56 +0930

    Update full_text_searchable_concern.rb

--- app/models/concerns/full_text_searchable_concern.rb
+++ app/models/concerns/full_text_searchable_concern.rb
@@ -4,9 +4,9 @@ module FullTextSearchableConcern
   included do
     normalizes :full_text, with: StrippedHtmlNormalizer.new
 
-    scope :search, ->(term) {
-      where("to_tsvector(#{table_name}.pg_text_search_configuration, #{table_name}.full_text) @@ to_tsquery(?)", string_to_query(term))
-    }
+  scope :search, ->(term) {
+    where("to_tsvector(#{table_name}.pg_text_search_configuration, #{table_name}.full_text) @@ websearch_to_tsquery(?)", term)
+  }
   end
 
   class_methods do
